3882008590a25c4c91a1ed13ad4c63928742f266
Manav Gosain <mu@Manavs-MacBook-Pro.local>
2024-05-14 01:12:45 +0530

    text-center on hero text

--- app/components/hero/page.tsx
+++ app/components/hero/page.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <section className="overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2 sm:items-center">
       <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-        <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <div className="mx-auto max-w-xl text-start ltr:sm:text-left rtl:sm:text-right">
           <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit
           </h2>
